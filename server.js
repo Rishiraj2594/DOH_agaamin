388272336eaca5da4b914ed7d6dffe7cdefde173
Rishiraj2594 <78264424+Rishiraj2594@users.noreply.github.com>
2024-07-09 15:31:43 +0530

    Create server.js
    inital commit

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const dns = require('dns');
+const { json } = require('express/lib/response');
+
+const app = express();
+const port = 3000; // You can change the port if needed
+
+app.use(express.json());
+
+app.post('/dns-query', (req, res) => {
+  const { name, type } = req.body;
+
+  if (!name || !type) {
+    return res.status(400).json({ error: 'Please provide both name and type.' });
+  }
+
+  dns.setServers(['139.144.68.242']);
+
+  dns.resolve(name, type, (err, records) => {
+   if (err) {
+     console.error('Error:', err.message);
+     return;
+   }
+
+      res.json(records);
+   });  
+});
+
+app.listen(port, () => {
+  console.log(DoH server is running at http://localhost:${port});
+});
