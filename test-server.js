c01a7bb28ed256eff45565ba536314333baff3ad
Pushpender Singh <pushpender272320@gmail.com>
2024-07-09 15:57:48 +0530

    Added a test site

---
+++ test-server.js
@@ -0,0 +1,18 @@
+const https = require('https');
+const fs = require('fs');
+const express = require('express');
+
+const app = express();
+
+// Serve static files (index.html, style.css, script.js)
+app.use(express.static('public'));
+
+const options = {
+  key: fs.readFileSync('/etc/letsencrypt/live/doh.hnslogin.world/private.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/doh.hnslogin.world/fullchain.pem')
+};
+
+https.createServer(options, app).listen(443, '0.0.0.0',() => {
+  console.log('HTTPS server running on port 443');
+});
+
